aa1d9c6f655377ceb99fbf22db811607d2763e70
Tyler Pulse <tyler3321@gmail.com>
2017-10-11 19:46:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,4 +33,10 @@ def turn(board)
   user_input = gets.chomp
   index = input_to_index(user_input)
 
+  if valid_move?(board, index)
+    move(board, index)
+    display_board
+  else
+    turn(board)
+  end
 end
