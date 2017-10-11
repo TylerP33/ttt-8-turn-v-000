bd413d42e56ee798a9735e549766f2d32fe3b804
Tyler Pulse <tyler3321@gmail.com>
2017-10-11 18:47:00 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def valid_move?(board, index)
+  # if index.between?(1,9)
+  #   if !position_taken?(board, index)
+  #     true
+  #   end
+  # end
+
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def turn(user_input)
+  puts "Please enter 1-9:"
+  user_input = gets.chomp
+  input_to_index(user_input)
+
+  if user_input == (1..9)
+    puts display_board
+  else
+    puts "Please enter 1-9:"
+  end
+
+
+
+end
