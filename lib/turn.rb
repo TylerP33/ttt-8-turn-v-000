40d0dcfdd4b9d123534e9ec1d3bdfb0d9e845c90
Tyler Pulse <tyler3321@gmail.com>
2017-10-11 18:52:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,9 +34,11 @@ def turn(user_input)
   input_to_index(user_input)
 
   if user_input == (1..9)
-
+    display_board
   else user_input != (1..9)
+    
     puts "Please enter 1-9:"
+    
   end
 
 
